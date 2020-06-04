13c4b36417a322355f077288e713df9baafec23a
ugurduyar <ismailugurduyar@gmail.com>
2020-06-04 23:13:16 +0300

    video current time added

--- script.js
+++ script.js
@@ -6,13 +6,37 @@ const progress = document.getElementById("progress");
 
 // Play & pause video
 function toggleVideoStatus() {
-  return true;
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
 // Update play pause icon
 
 function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+// Update progress & timestamp
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+}
+
+// Set video time to progress
+function setVideoProgress() {
   return true;
+}
+
+// Stop video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
 }
 
 // Event listeners
